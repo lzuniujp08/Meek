fda932ac4eeb39bae8fef5e2ca69d4c8521b428e
zybeargithub <1106408264@qq.com>
2017-05-25 09:28:09 +0800

    continue to complete select component

--- src/components/SelectCpt.js
+++ src/components/SelectCpt.js
@@ -5,6 +5,15 @@
 import Component from './Component'
 
 import BrowserEvent from '../meek/BrowserEvent'
+import FeaureLayer from '../lyr/FeatureLayer'
+import {listen, unlistenByKey} from '../core/EventManager'
+import {EventType} from '../meek/EventType'
+import {Style} from '../style/Style'
+
+import FillStyle from '../style/FillStyle'
+import LineStyle from '../style/LineStyle'
+
+import Feature from '../meek/Feature'
 
 
 export default class SelectCpt extends Component {
@@ -17,35 +26,107 @@ export default class SelectCpt extends Component {
     
     this.selectMode = opt.selectMode ? opt.selectMode : BrowserEvent.SINGLE_CLICK
     
-    this._hitTolerance = opt.hitTolerance ? opt.hitTolerance : 0
+    this._hitTolerance = opt.hitTolerance ? opt.hitTolerance : 2
+  
+  
+    /**
+     * 初始化草稿图层，用于临时高亮显示绘制的图形
+     *
+     * @type {FeatureLayer}
+     * @private
+     */
+    this._selectLayer = new FeaureLayer()
+    // this._selectLayer.style = this.getDefaultStyleFunction()
+    
+    this._selectFeatures = []
     
   }
   
+  _condition (event) {
+    return this.selectMode === event.type
+  }
   
   handleMouseEvent (browserEvent) {
-    if (!this.selectMode(browserEvent)) {
+    if (!this._condition(browserEvent)) {
       return
     }
     
     const map = browserEvent.map
-    const piexl = browserEvent.piexl
+    const pixel = browserEvent.pixel
     const hitTolerance = this._hitTolerance
-    const layers = map.layers
     
-    layers.forEach(function(layer) {
-      layer.forEachFeatureAtPiexl(piexl, function() {
-        
-      }, hitTolerance)
-    })
+    this._selectLayer.clear()
     
-    
-    
-    
-    
-    
-    
-    
-    
+    map.forEachFeatureAtPiexl(pixel, (function(features) {
+      if(features.length > 0){
+        this.selectFeatures = features
+        this.getHightLighStyleForFeature()
+        this._selectLayer.addFeatures(features)
+      }
+    }).bind(this), hitTolerance)
   }
   
+  set selectFeatures (features) {
+    features.forEach( feature =>
+      this._selectFeatures.push(feature.clone())
+    )
+  }
+  
+  get selectFeatures () { return this._selectFeatures }
+  
+  /**
+   * Update the drawing state for aborting drawing if active is false
+   * @private
+   */
+  _updateState () {
+    const map = this.map
+    const active = this.active
+    if (!map || !active){
+      // this._abortDrawing()
+    }
+    
+    this._selectLayer.map = active ? map : null
+  }
+  
+  getHightLighStyleForFeature () {
+    const features = this.selectFeatures
+    features.forEach (function(feature) {
+      const styles = feature.style
+      styles.forEach (function(style){
+        if (style instanceof FillStyle ) {
+          style.alpha = style.alpha + 0.2
+          style.borderStyle.width = style.borderStyle.width + 2
+        } else if (style instanceof LineStyle) {
+          style.width = style.width + 2
+        }
+      })
+    })
+  }
+  
+  set map (map) {
+    if (this._mapRenderKey) {
+      unlistenByKey(this._mapRenderKey)
+      this._mapRenderKey = null
+    }
+    
+    if (map) {
+      this._map = map
+      this._mapRenderKey = listen(this, EventType.CHANGE, map.render, map)
+    }
+    
+    this._updateState()
+  }
+  
+  get map (){ return this._map }
+  
+  /**
+   * Get the default style which will be used while a feature is drawn
+   * @returns {Function}
+   */
+  getDefaultStyleFunction () {
+    const styles = Style.createDefaultEditing()
+    return function (feature) {
+      return styles[feature.geometry.geometryType]
+    }
+  }
 }
