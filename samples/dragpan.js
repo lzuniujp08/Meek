59745d3847762e82fb4ceb5b4de152b6a226b715
zybeargithub <1106408264@qq.com>
2017-06-05 14:50:35 +0800

    Features generating use static initialization instead of drawing

--- samples/dragpan.js
+++ samples/dragpan.js
@@ -6,35 +6,34 @@
  */
 
 window.onload = function () {
-
+  
+  var point = new Datatang.Point(200,700)
+  
+  var path = [[400,200],[500,300],[500,50],[80,600]]
+  var line = new Datatang.Line()
+  line.path = path
+  
+  var rings = [[[500,400],[490,478],[350,350],[500,400]]]
+  var polygon = new Datatang.Polygon(rings)
+  
+  var extent = new Datatang.Extent(1100, 300, 1400, 600)
+  
+  var features = [new Datatang.Feature(point),
+    new Datatang.Feature(line),
+    new Datatang.Feature(polygon),
+    new Datatang.Feature(extent)]
+  
   // 将会获取缺省样式
-  var flayer = new Datatang.FeatureLayer()
-
+  var tempLayer = new Datatang.FeatureLayer({features: features})
   var map = new Datatang.Map({
-    layers: [flayer],
+    layers: [tempLayer],
     target: 'map'
   })
-
-  // 绘图工具
-  var drawTool = new Datatang.DrawCpt({
-    type: 'point',
-    drawLayer: flayer
-  })
-
-  map.addComponents(drawTool)
-
-  var typeSelect = document.getElementById('type')
-
-  /**
-   * Handle change event.
-   */
-  typeSelect.onchange = function() {
-    drawTool.drawMode = typeSelect.value
-  }
 
   var dragPan = new Datatang.DragPanCpt({
     kinetic: new Datatang.Kinetic(-0.005, 0.05, 100)
   })
+  
   map.addComponents(dragPan)
 
 }
