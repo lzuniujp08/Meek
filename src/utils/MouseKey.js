6f170d2e609a7ef2fec45d4e2a6502835f606f81
zybeargithub <1106408264@qq.com>
2017-06-07 09:28:22 +0800

    add functions that serves for mousewheel events.

--- src/utils/MouseKey.js
+++ src/utils/MouseKey.js
@@ -2,7 +2,6 @@
  * Created by zhangyong on 2017/5/23.
  */
 
-
 import BrowserEvent from '../meek/BrowserEvent'
 
 
@@ -10,6 +9,10 @@ export function singleClick (browserEvent) {
   return browserEvent.type === BrowserEvent.SINGLE_CLICK
 }
 
+export function mouseWheel (browserEvent) {
+  return browserEvent.type === BrowserEvent.MOUSE_WHEEL ||
+         browserEvent.type === BrowserEvent.WHEEL
+}
 
 export function noModifierKeys (browserEvent) {
   const originalEvent = browserEvent.originalEvent
