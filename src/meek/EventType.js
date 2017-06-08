e6f3ace24b3f8c3284a31ec7413fef24d6f44ae2
zybeargithub <1106408264@qq.com>
2017-06-08 10:12:05 +0800

    add image events for image downloading.

--- src/meek/EventType.js
+++ src/meek/EventType.js
@@ -1,24 +1,31 @@
 /**
  * Created by zypc on 2016/12/6.
  */
-export const EventType = {
-}
+export const EventType = {}
 
+/**
+ * Triggered when map is needed to render
+ * @type {string}
+ */
 EventType.CHANGE = 'change'
 
-// EventType.MOUSEDOWN = 'mousedown'
-//
-// EventType.MOUSEMOVE = 'mousemove'
-//
-// EventType.MOUSEUP = 'mouseup'
-//
-// EventType.MOUSEOVER = 'mouseover'
-//
-// EventType.MOUSEOUT = 'mouseout'
-//
-// EventType.DBLCLICK = 'dblclick'
-//
-// EventType.SINGLECLICK = 'singleclick'
+/**
+ * Triggered when a image download error
+ * @type {string}
+ */
+EventType.ERROR = 'error'
+
+/**
+ * Triggered when a image finish load
+ * @type {string}
+ */
+EventType.LOAD = 'load'
+
+
+export default {
+  EventType
+}
+
 
 
 
