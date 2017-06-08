e8969cd77e9c3633a80e4d35c0fbe7bf601c9cd2
zybeargithub <1106408264@qq.com>
2017-06-08 10:10:45 +0800

    update the entrance file.

--- src/main.js
+++ src/main.js
@@ -2,22 +2,33 @@
  * Created by zypc on 2016/11/28.
  */
 
+import {Config} from 'meek/Config'
+import Counter from 'utils/Counter'
+import Helpers from 'utils/Helpers'
+import {Obj} from 'utils/Obj'
+import {Math} from 'utils/Math'
+import {Coordinate} from 'utils/Coordinate'
+import {Canvas} from 'utils/Canvas'
+
+import {GeometryUtil} from 'geometry/support/GeometryUtil'
+import {ExtentUtil} from 'geometry/support/ExtentUtil'
+import {EventType} from 'meek/EventType'
+import {Style} from 'style/Style'
+
 import Event from 'core/Event'
 import EventManager from 'core/EventManager'
 import BaseObject from 'core/BaseObject'
-import Counter from 'utils/Counter'
-import Helpers from 'utils/Helpers'
-import Obj from 'utils/Obj'
 
 import Geometry from 'geometry/Geometry'
 import Extent from 'geometry/Extent'
 import Point from 'geometry/Point'
 import Line from 'geometry/Line'
 import Polygon from 'geometry/Polygon'
-import GeometryUtil from 'geometry/support/GeometryUtil'
-import ExtentUtil from 'geometry/support/ExtentUtil'
 
-import Style from 'style/Style'
+import {ImageState} from 'lyr/image/ImageState'
+import {ImageEvent} from 'lyr/image/ImageEvent'
+import SingleImage from 'lyr/image/SingleImage'
+
 import PointStyle from 'style/PointStyle'
 import LineStyle from 'style/LineStyle'
 import FillStyle from 'style/FillStyle'
@@ -32,20 +43,27 @@ import SelectCpt from 'components/SelectCpt'
 import ModifyCpt from 'components/ModifyCpt'
 import DragPanCpt from 'components/DragPanCpt'
 import Kinetic from 'components/Kinetic'
+import MouseWheelZoom from 'components/MouseWheelZoom'
 
 
 import BrowserEvent from 'meek/BrowserEvent'
 import BrowserEventHandler from 'meek/BrowserEventHandler'
-import EventType from 'meek/EventType'
 import Feature from 'meek/Feature'
 import Map from 'meek/Map'
+import View from 'meek/View'
 
 export default {
+  Config,
+  Coordinate,
+  Canvas,
   Event,
   EventManager,
   BaseObject,
   Counter,
   Helpers,
+  ImageState,
+  ImageEvent,
+  SingleImage,
   Geometry,
   Extent,
   Point,
@@ -67,11 +85,14 @@ export default {
   EventType,
   Feature,
   Map,
+  View,
   GeometryUtil,
   ExtentUtil,
   SelectCpt,
   ModifyCpt,
   DragPanCpt,
   Kinetic,
-  Obj
+  Obj,
+  Math,
+  MouseWheelZoom
 }
