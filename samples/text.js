9a920e26f06dc9f49503a1a204ca113ae724e6b0
zybeargithub <1106408264@qq.com>
2017-06-13 10:26:30 +0800

    add text style sample for geometry render.

---
+++ samples/text.js
@@ -0,0 +1,55 @@
+/**
+ * Created by zhangyong on 2017/6/12.
+ */
+
+
+window.onload = function () {
+  
+  var point = new Datatang.Point(900,500)
+  
+  var path = [[400,200],[500,300],[500,50],[80,600]]
+  var line = new Datatang.Line()
+  line.path = path
+  
+  var rings = [[[500,400],[490,478],[350,350],[500,400]]]
+  var polygon = new Datatang.Polygon(rings)
+  
+  var extent = new Datatang.Extent(1100, 300, 1400, 600)
+  
+  var features = [new Datatang.Feature(point),
+    new Datatang.Feature(line),
+    new Datatang.Feature(polygon),
+    new Datatang.Feature(extent)]
+  
+  // 将会获取缺省样式
+  var featureLayer = new Datatang.FeatureLayer({
+    features: features
+  })
+  
+  var mapextent = [0, 0, 1024, 968];
+  var map = new Datatang.Map({
+    layers: [
+      new Datatang.SingleImageLayer({
+        url: 'source/China_map.jpg',
+        imageExtent: extent,
+        projection: {
+          extent: extent
+        }
+      }),
+      featureLayer
+    ],
+    target: 'map',
+    view: new Datatang.View({
+      projection: {
+        extent: mapextent
+      },
+      center: Datatang.ExtentUtil.getCenter(mapextent),
+      zoom: 2,
+      maxZoom: 8
+    })
+  });
+  
+  
+  
+  
+}
