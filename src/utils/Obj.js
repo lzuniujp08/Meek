e21f4d06ed4d77c33f35278f719bd3fe5fb5a18a
zybeargithub <1106408264@qq.com>
2017-06-14 09:32:27 +0800

    Change to a static class and add a static function.

--- src/utils/Obj.js
+++ src/utils/Obj.js
@@ -2,23 +2,44 @@
  * Created by zhangyong on 2017/6/5.
  */
 
-export const Obj = {}
-
-Obj.getValues = function (object) {
-  const values = []
-  for (let property in object) {
-    values.push(object[property])
+export default class Obj {
+  
+  /**
+   * Get a collection from object
+   * @param object
+   * @returns {Array}
+   */
+  static getValues (object) {
+    const values = []
+    for (let property in object) {
+      values.push(object[property])
+    }
+    return values
   }
-  return values
-}
-
-Obj.clear = function (object) {
-  for (let property in object) {
-    delete object[property]
+  
+  /**
+   * Clear all properties in object
+   * @param object
+   */
+  static clear (object) {
+    for (let property in object) {
+      delete object[property]
+    }
+  }
+  
+  /**
+   * Convert an object to a map instance
+   * @param obj
+   * @returns {Map}
+   */
+  static objectToMap (obj) {
+    let map = new Map()
+    
+    for (let ke of Object.keys(obj)) {
+      map.set(ke, obj[ke])
+    }
+    
+    return map
   }
 }
 
-
-export default {
-  Obj
-}
