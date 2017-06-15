74f68a4d40b5c9db19fd57594a4f7f171ff89b08
zybeargithub <1106408264@qq.com>
2017-06-15 11:31:19 +0800

    add coordinate system sample.

---
+++ samples/coordinate-system.js
@@ -0,0 +1,44 @@
+/**
+ * Created by zhangyong on 2017/6/15.
+ */
+
+window.onload = function () {
+  
+  var point = new Datatang.Point(100, 100)
+  
+  var features = [
+    new Datatang.Feature(point)
+  ]
+  
+  // 将会获取缺省样式
+  var featureLayer = new Datatang.FeatureLayer({
+    features: features
+  })
+  
+  
+  var mapextent = [0, 0, 2783, 2125];
+  var map = new Datatang.Map({
+    layers: [
+      new Datatang.SingleImageLayer({
+        url: 'source/China_map.jpg',
+        imageExtent: mapextent,
+        projection: {
+          extent: mapextent
+        }
+      }),
+      featureLayer
+    ],
+    target: 'map',
+    view: new Datatang.View({
+      projection: {
+        extent: mapextent
+      },
+      center: Datatang.ExtentUtil.getCenter(mapextent),
+      zoom: 2,
+      maxZoom: 8
+    })
+  });
+  
+}
+
+
