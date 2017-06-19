dfdd2f9ff85cd483fe324f332d3737e11bd1b076
Peachisland <guojinghao20ni@163.com>
2017-06-19 15:57:21 +0800

    add example for 'draw and select features', selected 'None' will change to select mode

---
+++ samples/draw-and-select-features.js
@@ -0,0 +1,66 @@
+/**
+ * Created by guojing on 2017/6/19.
+ */
+var drawTool,
+    selectTool,
+    modifyTool;
+
+window.onload = function (){
+  var Fortesting = new Datatang.FeatureLayer()
+  var extent = [0,0,1280,800]
+
+  var map = new Datatang.Map({
+    layers:[new Datatang.SingleImageLayer({
+      url : "source/image01450.jpg",
+      imageExtent :extent,
+      projection:{
+        extent: extent
+      }
+    }),
+      Fortesting
+    ],
+    target:'map',
+    view : new Datatang.View({
+      projection:{
+        extent:extent
+      },
+      center: Datatang.ExtentUtil.getCenter(extent),
+      zoom: 2,
+      maxZoom: 8
+    })
+  })
+
+  drawTool = new Datatang.DrawCpt({
+    type: 'point',
+    drawLayer: Fortesting
+  });
+
+  modifyTool = new Datatang.ModifyCpt({
+    features: Fortesting.features
+  });
+
+  selectTool = new Datatang.SelectCpt({
+    selectMode: Datatang.BrowserEvent.CLICK
+  });
+
+  map.addComponents(drawTool)
+  map.addComponents(selectTool)
+  map.addComponents(modifyTool)
+
+  var typeSelect = document.getElementById('type')
+
+  typeSelect.onchange = function(){
+    if(typeSelect.value === "None"){
+      drawTool.active = false
+      selectTool.active = true
+      modifyTool.active = true
+    }else{
+
+     drawTool.active = true
+     drawTool.drawMode = typeSelect.value
+     selectTool.active = false
+     modifyTool.active = false
+    }
+  }
+
+}
