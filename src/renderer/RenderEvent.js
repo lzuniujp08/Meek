04f20d005bf7f5d351afa4e9fc9a9188d3626d51
zybeargithub <1106408264@qq.com>
2017-06-27 15:04:15 +0800

    add RenderEvent class as an event dispatched while map rendering.

--- src/renderer/RenderEvent.js
+++ src/renderer/RenderEvent.js
@@ -1,3 +1,31 @@
 /**
  * Created by zhangyong on 2017/6/27.
  */
+
+import BaseEvent from '../core/BaseEvent'
+
+export default class RenderEvent extends BaseEvent {
+  
+  constructor (type, frameState, context, trasform) {
+    
+    super(type)
+  
+  
+    /**
+     *
+     */
+    this.frameState = frameState
+  
+  
+    /**
+     *
+     */
+    this.trasform = trasform
+  
+    /**
+     *
+     */
+    this.context = context
+  }
+  
+}
