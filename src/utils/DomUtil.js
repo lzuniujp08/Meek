ba8c53fb873f1408f77238b71b5250dc440cc8d8
zybeargithub <1106408264@qq.com>
2017-06-27 14:53:21 +0800

    add some usefull functions

--- src/utils/DomUtil.js
+++ src/utils/DomUtil.js
@@ -2,6 +2,12 @@
  * Created by zhangyong on 2017/3/17.
  */
 
+/**
+ *
+ * @param optWidth
+ * @param optHeight
+ * @returns {CanvasRenderingContext2D|WebGLRenderingContext}
+ */
 export function createCanvasContext2D (optWidth, optHeight) {
   let canvas = document.createElement('CANVAS')
   if (optWidth) {
@@ -15,7 +21,45 @@ export function createCanvasContext2D (optWidth, optHeight) {
   return canvas.getContext('2d')
 }
 
+/**
+ *
+ * @param element
+ * @returns {number}
+ */
+export function outerWidth (element) {
+  let width = element.offsetWidth
+  const style = getComputedStyle(element)
+  width += parseInt(style.marginLeft, 10) + parseInt(style.marginRight, 10)
+  
+  return width
+}
+
+/**
+ *
+ * @param element
+ * @returns {number}
+ */
+export function outerHeight (element) {
+  let height = element.offsetHeight
+  const style = getComputedStyle(element)
+  height += parseInt(style.marginTop, 10) + parseInt(style.marginBottom, 10)
+  
+  return height
+}
+
+/**
+ *
+ * @param node
+ */
+export function removeChildren (node) {
+  while (node.lastChild) {
+    node.removeChild(node.lastChild)
+  }
+}
 
 export default {
-  createCanvasContext2D
+  createCanvasContext2D,
+  outerWidth,
+  outerHeight,
+  removeChildren
 }
