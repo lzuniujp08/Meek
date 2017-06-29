384972994e7d68947205d94c0230aa498a09bc0a
zybeargithub <1106408264@qq.com>
2017-06-29 14:26:10 +0800

    add api annotations

--- src/core/BaseEvent.js
+++ src/core/BaseEvent.js
@@ -2,26 +2,44 @@
  * Created by zhangyong on 2017/5/22.
  */
 
-
+/**
+ * The BaseEvent is a base class, all event dinfined in library should inherit this. <br/>
+ * The sub event class can define it's own property , <br/>
+ * which can be passed to other event Listener via event dispatch.<br/>
+ *
+ * 定义事件模型基类，自定义事件必须继承该类
+ *
+ * @class BaseEvent
+ * @constructor
+ * @module core
+ *
+ */
 export default class BaseEvent {
   
+  /**
+   * Create a BaseEvent
+   * @param type
+   */
   constructor (type) {
   
     /**
-     *
+     * 是否停止事件的冒泡
      * @type {boolean}
+     * @property propagatinoStopped
      */
     this.propagatinoStopped = false
   
     /**
      * The event type.
      * @type {string}
+     * @property type
      */
     this.type = type
   
     /**
      * The event target.
      * @type {Object}
+     * @property target
      */
     this.target = null
   }
@@ -29,7 +47,11 @@ export default class BaseEvent {
 }
 
 /**
+ * stop propagation from dom event
  * @param {Event|Event} evt Event
+ * @static
+ * @method stopPropagation
+ * @param {Object} evt DOM event model
  */
 BaseEvent.stopPropagation = function(evt) {
   evt.stopPropagation()
@@ -37,7 +59,11 @@ BaseEvent.stopPropagation = function(evt) {
 
 
 /**
+ * Prevent the default action from browser
  * @param {Event|Event} evt Event
+ * @static
+ * @method preventDefault
+ * @param {Object} evt DOM event model
  */
 BaseEvent.preventDefault = function(evt) {
   evt.preventDefault()
--- src/core/BaseObject.js
+++ src/core/BaseObject.js
@@ -7,13 +7,21 @@ import {getUid} from '../utils/Counter'
 import {EventType} from '../meek/EventType'
 
 /**
- * The base object that only can be inherited,
- * It will
+ * The BaseObject is a base class that only can be inherited, not be instanced.<br/>
+ *
+ * 对象基类，所有Meek中的子类原则上要继承该类。<br/>
+ * 该类提供一个 id 和一个 revision,
+ * id 用于作为类的ID标示
+ *
+ * @class BaseObject
+ * @extends Event
+ * @constructor
+ * @module core
  */
 export default class BaseObject extends Event {
 
   /**
-   * 本对象只能被继承,不能直接实例化使用
+   * Create a BaseObject
    * @consructor
    */
   constructor () {
@@ -35,27 +43,32 @@ export default class BaseObject extends Event {
   }
   
   /**
-   * Dispatch a 'change' event and the
-   * map will render the frame.
+   * Dispatch a 'change' event and the map will render the frame.
+   * <br/>
+   * 此方法用于派发map渲染事件
+   *
+   * @method changed
    */
   changed () {
     ++this._revision
     this.dispatchEvent(EventType.CHANGE)
-    
-    // console.log('revision:' + this._revision)
   }
 
   /**
-   * 获取对象的ID
-   * @returns {string}
+   * get the id value <br/>
+   * ID属性，用于获取该对象的ID编号
+   * @property id
+   * @type Number
    */
   get id () {
     return this._id
   }
   
   /**
-   *
-   * @returns {number}
+   * get the revision value
+   * @property revision
+   * @type Number
+   * @returns {number} the revision id
    */
   get revision () { return this._revision }
   
