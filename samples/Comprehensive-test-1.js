a17f9cf3461991ed1b44db4cedb13c25769f2c11
593949622 <593949622@qq.com>
2017-07-13 09:41:38 +0800

    add Comprehensive-test-1.html to testing

--- samples/Comprehensive-test-1.js
+++ samples/Comprehensive-test-1.js
@@ -162,7 +162,7 @@ window.onload = function () {
   function removeClass(obj, cls) {
     if (hasClass(obj, cls)) {
       var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-      obj.className = obj.className.replace(reg, ' ');
+      obj.className = obj.className.replace(reg, '');
     }
   }
 
