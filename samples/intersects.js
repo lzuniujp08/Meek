1465c2e3efb673b5115275f2ac4c0d368c73852f
zybeargithub <1106408264@qq.com>
2017-07-13 16:17:11 +0800

    add the geometry intersection sample

--- samples/intersects.js
+++ samples/intersects.js
@@ -1,3 +1,77 @@
 /**
  * Created by zhangyong on 2017/7/13.
  */
+
+
+
+// 将会获取缺省样式
+var flayer = new Datatang.FeatureLayer()
+
+var extent = [0, 0, 2783, 2125];
+
+var map = new Datatang.Map({
+  layers: [
+    new Datatang.SingleImageLayer({
+      url: 'source/China_map.jpg',
+      imageExtent: extent,
+      projection: {
+        extent: extent
+      }
+    }),
+    flayer
+  ],
+  target: 'map',
+  view: new Datatang.View({
+    projection: {
+      extent: extent
+    },
+    center: Datatang.ExtentUtil.getCenter(extent),
+    zoom: 2,
+    maxZoom: 8
+  })
+});
+
+// 绘图工具
+var draw = new Datatang.Draw({
+  type: 'point',
+  drawLayer: flayer
+})
+
+map.addComponents(draw)
+
+var typeSelect = document.getElementById('type')
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function() {
+  draw.drawMode = typeSelect.value
+}
+
+
+// 多边形
+var rings = [[800,580],[490,600],[400,660],[300, 700],[270, 780],[255, 820],[230, 860],[280,1050],
+  [1000,1000],[1000,800],[800,580]]
+var polygon = new Datatang.Polygon(rings)
+var feature = new Datatang.Feature(polygon)
+
+flayer.addFeature(feature)
+
+draw.addEventListener(Datatang.DrawEvent.EventType.DRAW_END, function(e){
+  var feature = e.feature
+  
+  var geometry = feature.geometry
+  var info = ''
+  
+  var result = Datatang.intersects(geometry, polygon)
+  if(result) {
+    info = '相交！'
+  } else {
+    info = '不相交'
+  }
+  
+  
+  
+  alert(info)
+  
+})
