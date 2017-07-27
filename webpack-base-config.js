2a827b0ff0ca341ac13bee9b8f420e5335100167
zybeargithub <1106408264@qq.com>
2017-07-27 11:33:30 +0800

    增加release命令用于生产环境min包生成

--- webpack-base-config.js
+++ webpack-base-config.js
@@ -7,13 +7,6 @@ module.exports = {
     new webpack.DefinePlugin({
       VERSION: JSON.stringify(require('./package.json').version)
     })
-    // ,
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //     drop_console: true
-    //   },
-    // })
   ],
   module: {
     loaders: [
